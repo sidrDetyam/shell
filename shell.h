0c8a667afa95798748becfc044fa5095ab7244df
Alex <a.gemuev@g.nsu.ru>
2022-05-22 02:15:20 +0700

    added command editing and history
    command editing is done with non-canonical terminal input

--- shell.h
+++ shell.h
@@ -3,14 +3,18 @@
 #define SHELL
 
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
 #include "processtypes.h"
 
 int parse_line(char* line, Job** jobs);
 
 #define MAX_CMD_LENGTH 4096
-int promptline(char *prompt, char *line, int sizline);
+int promptline(const char *prompt, char *line, vcharptr_t* history);
 
 #endif
